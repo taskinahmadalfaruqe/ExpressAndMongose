b3e1a70b88240dcd2aff3c1b68316f856ffcff49
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-01-25 21:40:22 +0800

    add backend Methods.

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
 {
+    // Insert Data 
+
+    // Insert Single Data in Database
+    db.PracticeData.insertOne({ name: "Taskin Ahmad", id: 15 })
+
+    //Insert Multiple Data. It must be an Array of obbject
+    db.PracticeData.insertMany([{ name: "Taskin Ahmad", id: 15 }, { name: "Taskin Ahmad", id: 15 }, { name: "Taskin Ahmad", id: 15 }])
+
+
+    // Find Data 
     
+    // Find Data Singel
+    db.PracticeData.findOne({ gender: "Female" })
+
+    // Find Multiple Data
+    db.PracticeData.find({ gender: "Male" })
 }
