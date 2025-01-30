8ded2b557319066bf4a96d4396b858d3394b5f38
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-01-30 23:38:18 +0800

    add New 2 Operator.

--- index.ts
+++ index.ts
@@ -9,10 +9,19 @@
 
 
     // Find Data 
-    
+
     // Find Data Singel
     db.PracticeData.findOne({ gender: "Female" })
 
     // Find Multiple Data
     db.PracticeData.find({ gender: "Male" })
+
+    // Find Value With Operators $EQ
+    db.Practicebackend.findOne({ gender: { $eq: "Female" } })
+    db.PracticeData.find({ gender: { $eq: "Male" } })
+
+    // Find Value With Operators $LEQ
+    db.Practicebackend.findOne({ age: { $lte: 12 } })
+    db.PracticeData.find({ age: { $gt: 55 } })
+
 }
