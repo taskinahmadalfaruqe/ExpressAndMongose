58054913589af3f555cc283c781be73ff9852fa2
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-02-01 20:45:40 +0800

    add Some Probloem In Mongodb.

--- index.ts
+++ index.ts
@@ -24,4 +24,60 @@
     db.Practicebackend.findOne({ age: { $lte: 12 } })
     db.PracticeData.find({ age: { $gt: 55 } })
 
+    // Find Value by sort with assending and desinding
+    db.BackendPractice.find({ age: { $lte: "50" } }).sort({ age: 1 }) //  for assending
+    db.BackendPractice.find({ age: { $lte: "50" } }).sort({ age: -1 }) //  for desending
+
+    // Fill Filtaring And  $in $nin
+    db.BackendPractice.find({ age: { $in: [10, 20, 30] } }).sort({ age: 1 }) // Only Return Match With age 10 20 30
+    db.BackendPractice.find({ age: { $nin: [10, 20, 30] } }).sort({ age: 1 }) // Only Return without  age 10 20 30
+}
+{
+    // Problem Solving With Data base
+
+    // 1. Find all documents in the collection where the age is greater than 30, and
+    // only return the name and email fields. 
+    db.PracticeBanckend.find({ age: { $gt: 30 } }, { name: 1 }).sort({ age: 1 }).project({ email: 1 })
+
+
+
+    // 2. Find documents where the favorite color is either "Maroon" or "Blue."
+    // Method 01 
+    db.PracticeBanckend.find({
+        $or: [
+            {
+                favoutiteColor: "Blue"
+            },
+            {
+                favoutiteColor: "Maroon"
+            },
+        ]
+    }).project({ favoutiteColor: 1 })
+
+    // Method 02
+    db.PracticeBanckend.find({ favoutiteColor: { $in: ["Blue", "Maroon"] } }).project({ favoutiteColor: 1 })
+
+
+    // 3. Find all documents where the skill is an empty array. 
+    db.PracticeBanckend.find({ skills: { $size: 0 } }).project({ skills: 1 })
+
+
+    // 4. Find documents where the person has skills in both "JavaScript" and "Java."c  
+    db.PracticeBanckend.find({ "skills.name": { $all: ["JAVASCRIPT", "JAVA"] } }).project({ "skills.name": 1 })
+
+    db.PracticeBanckend.find(
+        {
+            skills:
+            {
+                $elemMatch: {
+                    name: {
+                        $in:
+                            ["JAVASCRIPT", "JAVA"]
+                    }
+                }
+
+            }
+
+        }
+    ).project({ "skills.name": 1 })
 }
